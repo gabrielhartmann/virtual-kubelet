beb9cba7de167287275effdd41e1990b8203455b
Gabriel Hartmann <gabriel.hartmann@gmail.com>
2020-02-03 16:43:28 -0800

    Path NodeStatus conserving NodeConditions
    Resolves Issue #807
    Borrows technicque from node-problem-detector:
    https://github.com/kubernetes/node-problem-detector/blob/a442e71190e7d460c892474336c38a412b332be1/pkg/exporters/k8sexporter/problemclient/problem_client.go#L126-L133

--- node/node.go
+++ node/node.go
@@ -28,7 +28,6 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/strategicpatch"
 	"k8s.io/client-go/kubernetes/typed/coordination/v1beta1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
@@ -408,40 +407,18 @@ var emptyGetOptions = metav1.GetOptions{}
 
 // patchNodeStatus patches node status.
 // Copied from github.com/kubernetes/kubernetes/pkg/util/node
-func patchNodeStatus(nodes v1.NodeInterface, nodeName types.NodeName, oldNode *corev1.Node, newNode *corev1.Node) (*corev1.Node, []byte, error) {
-	patchBytes, err := preparePatchBytesforNodeStatus(nodeName, oldNode, newNode)
+func patchNodeStatus(nodes v1.NodeInterface, nodeName types.NodeName, newNode *corev1.Node) (*corev1.Node, []byte, error) {
+	raw, err := json.Marshal(&newNode.Status)
 	if err != nil {
 		return nil, nil, err
 	}
+	patchBytes := []byte(fmt.Sprintf(`{"status":%s}`, raw))
 
 	updatedNode, err := nodes.Patch(string(nodeName), types.StrategicMergePatchType, patchBytes, "status")
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to patch status %q for node %q: %v", patchBytes, nodeName, err)
 	}
 	return updatedNode, patchBytes, nil
-}
-
-func preparePatchBytesforNodeStatus(nodeName types.NodeName, oldNode *corev1.Node, newNode *corev1.Node) ([]byte, error) {
-	oldData, err := json.Marshal(oldNode)
-	if err != nil {
-		return nil, fmt.Errorf("failed to Marshal oldData for node %q: %v", nodeName, err)
-	}
-
-	// Reset spec to make sure only patch for Status or ObjectMeta is generated.
-	// Note that we don't reset ObjectMeta here, because:
-	// 1. This aligns with Nodes().UpdateStatus().
-	// 2. Some component does use this to update node annotations.
-	newNode.Spec = oldNode.Spec
-	newData, err := json.Marshal(newNode)
-	if err != nil {
-		return nil, fmt.Errorf("failed to Marshal newData for node %q: %v", nodeName, err)
-	}
-
-	patchBytes, err := strategicpatch.CreateTwoWayMergePatch(oldData, newData, corev1.Node{})
-	if err != nil {
-		return nil, fmt.Errorf("failed to CreateTwoWayMergePatch for node %q: %v", nodeName, err)
-	}
-	return patchBytes, nil
 }
 
 // updateNodeStatus triggers an update to the node status in Kubernetes.
@@ -464,7 +441,8 @@ func updateNodeStatus(ctx context.Context, nodes v1.NodeInterface, n *corev1.Nod
 		return nil, err
 	}
 
-	log.G(ctx).Debug("got node from api server")
+	log.G(ctx).WithField("node.Status.Conditions", oldNode.Status.Conditions).
+		Debug("got node from api server")
 	node = oldNode.DeepCopy()
 	node.ResourceVersion = ""
 	node.Status = n.Status
@@ -472,7 +450,7 @@ func updateNodeStatus(ctx context.Context, nodes v1.NodeInterface, n *corev1.Nod
 	ctx = addNodeAttributes(ctx, span, node)
 
 	// Patch the node status to merge other changes on the node.
-	updated, _, err := patchNodeStatus(nodes, types.NodeName(n.Name), oldNode, node)
+	updated, _, err := patchNodeStatus(nodes, types.NodeName(n.Name), node)
 	if err != nil {
 		return nil, err
 	}
